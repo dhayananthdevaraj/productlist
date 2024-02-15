c7545d4622237d73bfb1769d12753f60f17f3be2
neojarvis <techadmin@iamneo.ai>
2024-02-15 05:25:47 +0000

    Auto-save-commit-15/02/2024--10:55:45

--- angularapp/src/app/add-product/add-product.component.ts
+++ angularapp/src/app/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductService } from '../services/product.service';
 import { Router } from '@angular/router';
 import { Product } from '../model/product.model';
@@ -9,17 +11,39 @@ import { Product } from '../model/product.model';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-  product : Product ={};
+  productForm: FormGroup;
 
-  constructor(private productService: ProductService, private router: Router) { }
+  constructor(private fb: FormBuilder, private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
-  addProduct(){
-    this.productService.addProduct(this.product).subscribe(()=>{
-      console.log();
-      this.router.navigate(['/products']);
+
+  initForm() {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      category: ['', Validators.required],
+      price: [null, [Validators.required, Validators.min(0)]],
+      description: ['']
     });
   }
 
+  addProduct() {
+    this.validateAllFormFields();
+
+    if (this.productForm.valid) {
+      const newProduct: Product = this.productForm.value;
+      this.productService.addProduct(newProduct).subscribe(() => {
+        console.log('Product added successfully');
+        this.router.navigate(['/products']);
+      });
+    }
+  }
+
+  validateAllFormFields() {
+    Object.keys(this.productForm.controls).forEach(field => {
+      const control = this.productForm.get(field);
+      control.markAsTouched({ onlySelf: true });
+    });
+  }
 }
